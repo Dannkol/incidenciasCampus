e95d2c2e9da2711f73202784214e1a16dbef3226
Daniel Manosalva <manosalvasierra@gmail.com>
2023-07-16 00:16:34 -0500

    feat: :construction: filtrar equipos por marcas de usu componentes
    en este endpoint el resultado sera los equipos donde un accesorio coincida con la marca
    
    dannkol

--- src/models/equipos.js
+++ src/models/equipos.js
@@ -29,4 +29,29 @@ const getAllEquipos = async () => {
   }
 };
 
-export default { getAllEquipos };
+
+
+const getAllEquiposMarcas = async (data) => {
+    const connection = await getConnection();
+  
+    try {
+      const query_user = `SELECT t2.nombre as 'equipo', T4.marca as 'marca_diadema', T5.marca as 'marca_mouse', t6.marca as 'marca_teclado' ,t2.descripcion as 'descripcion_eq', t3.nombre as 'lugar' , t3.descripcion as 'desc_lugar' FROM equipos_acc_lugar AS T1
+      INNER JOIN equipos AS T2 ON T1.equipos_id = T2.id
+      INNER JOIN lugares AS T3 ON T2.lugar_id = T3.id
+      INNER JOIN accesorio_diademas AS T4 ON T1.accesorios_diademas_id = T4.id
+      INNER JOIN accesorio_mouse AS T5 ON T1.accesorios_mouse_id = T5.id
+      INNER JOIN accesorio_teclado AS T6 ON T1.accesorios_teclado_id = T6.id
+      WHERE T4.marca = ? OR T5.marca = ? OR T6.marca = ? ;`;
+  
+      const [result] = await connection.execute(query_user, [...data , ...data, ...data]);
+  
+      return result;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      connection.end();
+    }
+  };
+
+export default { getAllEquipos , getAllEquiposMarcas };
--- src/routes/routes_equipos.js
+++ src/routes/routes_equipos.js
@@ -2,7 +2,7 @@ import express from "express";
 
 // Controllers
 
-import { getAllEquipos } from "../controllers/equiposcontroller.js";
+import { getAllEquipos , getAllEquiposMarcas } from "../controllers/equiposcontroller.js";
 
 // Middlewareq
 import authenticateToken from "../middleware/auth_token_jwt.js";
@@ -11,6 +11,38 @@ import authenticateToken from "../middleware/auth_token_jwt.js";
 const router = express.Router();
 
 
-router.get("/", authenticateToken ,getAllEquipos)
+
+router.get("/", authenticateToken , getAllEquipos)
+
+// Get por marca
+// devuelve los equipos donde un accesorio
+// coicida con la marca
+// /api/equipos/Logitech
+// resultado
+// {
+//     "query": [
+//       {
+//         "equipo": "CP-123",
+//         "marca_diadema": "Logitech",
+//         "marca_mouse": "Logitech",
+//         "marca_teclado": "Logitech",
+//         "descripcion_eq": "Equipo en perfecto estado",
+//         "lugar": "apolo",
+//         "desc_lugar": "Salon de clase"
+//       },
+//       {
+//         "equipo": "CP-155",
+//         "marca_diadema": "Logitech",
+//         "marca_mouse": "Logitech",
+//         "marca_teclado": "Acer",
+//         "descripcion_eq": "Equipo en perfecto estado",
+//         "lugar": "apolo",
+//         "desc_lugar": "Salon de clase"
+//       }
+//     ]
+//  }
+
+router.get("/:marca", authenticateToken , getAllEquiposMarcas)
+
 
 export default router;
